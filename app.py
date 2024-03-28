994706f01dcb71847fc4cd3fc44a094d979c0aef
sushilshrestha <sshrest4@terpmail.umd.edu>
2024-03-27 20:46:49 -0400

    Edited app file to add new routes to demonstrate Flask Fundamentals

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -17,6 +17,37 @@ def about():
 @app.route('/about-css')
 def aboutcss():
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favoritecourse():
+    print('Subject:' + request.args.get('subject'))
+    print('Course Number:' + request.args.get('course_number'))
+    print('You entered your favorite course as:' + request.args.get('subject'))
+    return render_template('favorite-course.html')
+
+
+@app.route('/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'POST':
+        print('First name entered:'+request.form.get('first_name'))
+        print('Last name entered:'+request.form.get('last_name'))
+        print('Email entered:'+request.form.get('email'))
+        print('Favorite car entered:'+request.form.get('car'))
+
+    if request.form.get('agree_check'):
+        print('Agree to be contacted entered:' + request.form.get('agree_check'))
+
+    return render_template('contact.html')
+
+
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
